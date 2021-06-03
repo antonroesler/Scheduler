a43881dd8913fc817978bba6bc54b3ff6313f5b7
antonroesler <anton.roesler@stud.fra-uas.de>
2021-06-03 22:04:00 +0200

    first design of html page

--- TestScreen/views.py
+++ TestScreen/views.py
@@ -1,5 +1,7 @@
+import datetime
 import time
 
+import datetime as datetime
 from django.http.response import JsonResponse
 from django.shortcuts import render
 
@@ -11,12 +13,14 @@ from plotly.offline import plot
 import pandas as pd
 
 df = pd.DataFrame([
-    dict(Task="Job X", Start='2009-01-01', Finish='2009-02-28'),
-    dict(Task="Job B", Start='2009-03-05', Finish='2009-04-15'),
-    dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30')
+    dict(Task="Job X", Start=datetime.datetime(1970, 1, 1, 1, 1, 0), Finish=datetime.datetime(1970, 1, 1, 1, 1, 15)),
+    dict(Task="Job A", Start=datetime.datetime(1970, 1, 1, 1, 1, 15), Finish=datetime.datetime(1970, 1, 1, 1, 1, 19)),
+    dict(Task="Job B", Start=datetime.datetime(1970, 1, 1, 1, 1, 30), Finish=datetime.datetime(1970, 1, 1, 1, 1, 35)),
+    dict(Task="Job C", Start=datetime.datetime(1970, 1, 1, 1, 1, 19), Finish=datetime.datetime(1970, 1, 1, 1, 1, 30)),
+
 ])
 
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task")
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Task")
 fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
 plt_div = plot(fig, output_type='div', config=dict(
                     displayModeBar=False
