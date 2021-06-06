eb44df75bd74ad34934cef2d01d632d57ba7c2d7
antonroesler <anton.roesler@stud.fra-uas.de>
2021-06-06 20:29:11 +0200

    Added Algortihm name in title

--- TestScreen/static/testscreen/test.js
+++ TestScreen/static/testscreen/test.js
@@ -1,4 +1,22 @@
+function get_title(algo){
+    let title = "";
+    if (algo==="hrrn"){
+        title += "Highest Response Ratio Next"
+    } else if (algo==="sjf"){
+        title += "Shortest Job First"
+    } else if (algo==="srtf"){
+        title += "Shortest Remaining Time First"
+    } else if (algo==="lrtf"){
+        title += "Longest Remaining Time First"
+    } else if (algo==="rr"){
+        title += "Round Robin"
+    } else {
+        title += "First Come First Served"
+    }
+    title += " visualized:"
+    return title;
 
+}
 
 async function loadDiagram(){
     const url = 'http://127.0.0.1:8000/diagram/';
@@ -12,11 +30,11 @@ async function loadDiagram(){
     const lower = getHeight("add-section");
     plotly_data.layout.height = window.innerHeight - (upper+lower);
     console.log(plotly_data['layout']);
-
-    plotly_data['layout']['paper_bgcolor'] = "#2F2fA2";
+    plotly_data['layout']['title'] = get_title(algo);
+    plotly_data['layout']['paper_bgcolor'] = "#7575d7";
     plotly_data['layout']['plot_bgcolor'] = "#FFF";
     plotly_data['layout']['xaxis']['rangeselector'] = null;
-    plotly_data['layout']['template']['layout']['xaxis']['gridcolor'] = "#2F2fA2";
+    plotly_data['layout']['template']['layout']['xaxis']['gridcolor'] = "#7575d7";
     Plotly.react('divPlotly', plotly_data.data, plotly_data.layout, {responsive: true, displayModeBar: false});
 }
 
